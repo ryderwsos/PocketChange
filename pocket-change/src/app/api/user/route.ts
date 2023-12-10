4aaea9c8076b2145fd986aa94fc6be4fde577ef1
ryderwsos <86503143+ryderwsos@users.noreply.github.com>
2023-12-10 13:49:34 -0500

    included bcrypt

--- pocket-change/src/app/api/user/route.ts
+++ pocket-change/src/app/api/user/route.ts
@@ -2,10 +2,14 @@ import connect from "../../lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import User from "../model/userModel"
 import { NextRequest, NextResponse } from "next/server";
+const bcrypt = require('bcrypt')
 
 export async function POST(req: Request, res: NextApiResponse){
     const {username, password, email} = await req.json();
     await connect();
-    await User.create({username, password, email})
+
+    const encrypt_password = await bcrypt.hash(password, 10)
+
+    await User.create({username, encrypt_password, email})
     return NextResponse.json({username, password, email})
 }
