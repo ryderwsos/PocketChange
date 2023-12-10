48c9a71bae75a34de75922925f0350a05c8aed9e
smzalam <alam.syedmz@gmail.com>
2023-12-10 11:11:02 -0500

    implemented navbar sticky styling

--- pocket-change/src/app/components/NavMenu.tsx
+++ pocket-change/src/app/components/NavMenu.tsx
@@ -1,35 +1,112 @@
+'use client'
 import Link from "next/link";
 import { logo, body } from "../ui/fonts";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 export default function NavMenu() {
-  return (
-    <nav
-        className="sticky top-0 flex flex-row items-center justify-between mt-6 bg-primary rounded-md w-full"
-    >
-        <Link 
-            href='/'
-            className={`${logo.className} text-2xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-full h-full transition-all duration-500 ease-out`}
+    const [clientWindowHeight, setClientWindowHeight] = useState(0);
+    const [navColor, setNavColor] = useState('bg-primary');
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    });
+
+    const handleScroll = () => {
+        setClientWindowHeight(window.scrollY);
+    };
+
+    // useEffect(() => {
+    //     if (clientWindowHeight !== 0) {
+    //         setNavColor('bg-secondary')
+    //     } else {
+    //         setNavColor('bg-primary')
+    //     }
+    // }, [clientWindowHeight]);
+
+    return (
+        <div
+            className="sticky top-0 w-full"
         >
-            pocketchange
-        </Link>
-        <Link 
-            href='/discover'
-            className={`${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2 h-full transition-all duration-500 ease-out`}
-        >
-            Discover
-        </Link>
-        <Link 
-            href='/connections'
-            className={`${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2 h-full transition-all duration-500 ease-out`}
-        >
-            Connections
-        </Link>
-        <Link 
-            href='/inbox'
-            className={`${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2 h-full transition-all duration-500 ease-out`}    
-        >
-            Messages
-        </Link>
-    </nav>
-  )
+            <nav
+                className={
+                    clsx(
+                        "flex flex-row items-center justify-between mt-6 rounded-md w-full transition-all duration-500",
+                        {
+                            'bg-primary': clientWindowHeight === 0
+                        },
+                        {
+                            'bg-secondary': clientWindowHeight !== 0
+                        }
+                    )
+                }
+            >
+                <Link
+                    href='/'
+                    className={
+                        clsx(
+                            `${logo.className} text-2xl flex justify-center text-text rounded-md p-4 w-full h-full transition-all duration-500 ease-out`,
+                            {
+                                'hover:bg-secondary': clientWindowHeight === 0
+                            },
+                            {
+                                'hover:bg-primary': clientWindowHeight !== 0
+                            }
+                        )
+                    }
+                >
+                    pocketchange
+                </Link>
+                <Link
+                    href='/discover'
+                    className={
+                        clsx(
+                            `${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2 h-[64px] transition-all duration-500 ease-out`,
+                            {
+                                'hover:bg-secondary': clientWindowHeight === 0
+                            },
+                            {
+                                'hover:bg-primary': clientWindowHeight !== 0
+                            }
+                        )
+                    }
+                >
+                    Discover
+                </Link>
+                <Link
+                    href='/connections'
+                    className={
+                        clsx(
+                            `${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2  h-[64px] transition-all duration-500 ease-out`,
+                            {
+                                'hover:bg-secondary': clientWindowHeight === 0
+                            },
+                            {
+                                'hover:bg-primary': clientWindowHeight !== 0
+                            }
+                        )
+                    }
+                >
+                    Connections
+                </Link>
+                <Link
+                    href='/inbox'
+                    className={
+                        clsx(
+                            `${body.className} text-xl flex justify-center text-text hover:bg-secondary rounded-md p-4 w-1/2 h-[64px] transition-all duration-500 ease-out`,
+                            {
+                                'hover:bg-secondary': clientWindowHeight === 0
+                            },
+                            {
+                                'hover:bg-primary': clientWindowHeight !== 0
+                            }
+                        )
+                    }
+                >
+                    Messages
+                </Link>
+            </nav>
+        </div >
+    )
 }
--- pocket-change/src/app/page.tsx
+++ pocket-change/src/app/page.tsx
@@ -2,8 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 overflow-clip">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-[80vh] flex-col items-center justify-between p-24 w-full rounded-md">
+      <div className='flex flex-row basis-3/4'>
+        <h1>
+          Art
+        </h1>
+        <h1>
+          Networking
+        </h1>
+      </div>
+      <div className='basis-1/4'>
+        hi
+      </div>
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -107,7 +118,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
