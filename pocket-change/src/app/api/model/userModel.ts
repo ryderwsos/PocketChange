a32cc078e269489af5fc1b9c34e447fcdea8a55c
smzalam <alam.syedmz@gmail.com>
2023-12-11 02:01:41 -0500

    modified userModel to store tokens

--- pocket-change/src/app/api/model/userModel.ts
+++ pocket-change/src/app/api/model/userModel.ts
@@ -1,24 +1,37 @@
 import mongoose, { Schema } from 'mongoose';
+import { boolean } from 'webidl-conversions';
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Please provide a username!"],
         unique: true,
         trim: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Please provide an email!"],
         unique: true,
         trim: true
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Please provide a password!"],
         unique: true,
         trim: true
-    }
+    },
+    isVerified: {
+        type: boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
 })
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
